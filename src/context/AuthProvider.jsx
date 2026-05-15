d7cc33479bd5b8de1a1a9338c47e050acbc4c55d
unknown <mdrummanmondal2@gmail.com>
2026-05-16 01:10:03 +0600

    auth provider & context added

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AuthContext from './AuthContext';
+
+const AuthProvider = ({children}) => {
+
+     const createUser = ()=>{
+        crea
+     }
+
+    const authInfo = {
+
+    }
+    return <AuthContext value={authInfo}>{children}</AuthContext>
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Routes/Router.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Router.jsx";
+import AuthProvider from "./context/AuthProvider.jsx";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
-)
+);
